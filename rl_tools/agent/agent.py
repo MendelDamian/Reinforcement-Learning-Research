a55b56127da64f2acd8aaaa95a86331a96c6d6fc
Damian Mendel <mendeldamian01@gmail.com>
2023-05-09 11:08:22 +0200

    add agent

---
+++ rl_tools/agent/agent.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+from rl_tools.types import State, Action, Reward, Done
+
+
+class Agent(ABC):
+    @abstractmethod
+    def update(self, state: State, action: Action, reward: Reward, next_state: State, done: Done) -> None:
+        pass
+
+    @abstractmethod
+    def get_action(self, state: State, actions: tuple[Action, ...]) -> Action:
+        pass
+
+    @abstractmethod
+    def get_best_action(self, state: State, actions: tuple[Action, ...]) -> Action:
+        pass
